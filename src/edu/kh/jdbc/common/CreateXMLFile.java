134e01fd520b69f9d623fcda023ca21d9c037698
rbdus0515 <dnqkd707@gmail.com>
2023-08-16 17:26:30 +0900

    up2

--- src/edu/kh/jdbc/common/CreateXMLFile.java
+++ src/edu/kh/jdbc/common/CreateXMLFile.java
@@ -16,7 +16,7 @@ public class CreateXMLFile {
 		// XML 파일을 읽고, 쓰기 위한 IO 관련 클래스 필요
 		
 		// * Properties 컬렉션 객체 *
-		// - Map의 후소 클래스
+		// - Map의 후손 클래스
 		// - Key, Value 모두 String(문자열 형식)
 		// - XML 파일을 읽고, 쓰는데 특화된 메서드 제공
 		
